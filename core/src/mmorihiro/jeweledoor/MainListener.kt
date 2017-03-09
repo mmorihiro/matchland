c7b68883803e096caf40d739095d8642732ef70c
MMorihiro <motakesi54@gmail.com>
2017-03-09 09:47:55 +0900

    Show a bullet when the backGround is clicked(#1)

--- core/src/mmorihiro/jeweledoor/MainListener.kt
+++ core/src/mmorihiro/jeweledoor/MainListener.kt
@@ -2,35 +2,41 @@ package mmorihiro.jeweledoor
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.utils.viewport.FitViewport
+import com.badlogic.gdx.graphics.Texture
 import ktx.actors.onClick
 import ktx.actors.plus
+import ktx.assets.Assets
+import ktx.assets.load
+
 
 class MainListener : ApplicationAdapter() {
     lateinit var currentView: View
-    lateinit var stage: Stage
 
     override fun create() {
-        currentView = View().apply {
-            onClick { _, _, x, y -> println("x:$x, y: $y") }
-        }
+        Assets.manager = AssetManager()
+        load<Texture>("bullet.png")
+        Assets.manager.finishLoading()
 
-        stage = Stage(FitViewport(width, height)).apply {
-            this + currentView
+        currentView = View().apply {
+            backGround.onClick { _, _, x, y ->
+                println("clicked $x, $y")
+                shoot()
+            }
+            this + backGround
             Gdx.input.inputProcessor = this
         }
     }
 
     override fun render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        stage.act(Gdx.graphics.deltaTime)
-        stage.draw()
+        currentView.act(Gdx.graphics.deltaTime)
+        currentView.draw()
     }
 
     override fun dispose() {
-        stage.dispose()
+        currentView.dispose()
     }
 }
 
--- core/src/mmorihiro/jeweledoor/View.kt
+++ core/src/mmorihiro/jeweledoor/View.kt
@@ -1,10 +1,30 @@
 package mmorihiro.jeweledoor
 
-import com.badlogic.gdx.scenes.scene2d.Group
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.utils.viewport.FitViewport
+import ktx.actors.plus
+import ktx.assets.asset
 
-class View : Group() {
-    init {
-        setBounds(0f, 0f, width, height)
+class View : Stage(FitViewport(width, height)) {
+    private var bullets: List<Image> = listOf()
+
+    val backGround = object : Actor() {
+        override fun act(delta: Float) = stage.viewport.run {
+            width = screenWidth.toFloat()
+            height = screenHeight.toFloat()
+            x = screenX.toFloat()
+            y = screenY.toFloat()
+        }
+    }
+
+    fun shoot() {
+        bullets += Image(asset<Texture>("bullet.png")).apply {
+            setPosition(100f, 100f)
+            this@View + this
+        }
     }
 }
 
