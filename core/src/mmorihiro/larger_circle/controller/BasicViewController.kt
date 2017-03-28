956b06649da109b2e1073a8933958b82b5f7ffbe
MMorihiro <motakesi54@gmail.com>
2017-03-28 20:08:38 +0900

    Add CircularMotion(#18)

--- core/src/mmorihiro/larger_circle/controller/BasicViewController.kt
+++ core/src/mmorihiro/larger_circle/controller/BasicViewController.kt
@@ -1,7 +1,9 @@
 package mmorihiro.larger_circle.controller
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import ktx.actors.plus
+import mmorihiro.larger_circle.model.CircularMotion
 import mmorihiro.larger_circle.view.BasicView
 
 
@@ -10,6 +12,9 @@ class BasicViewController(bulletCounter: () -> Unit) : ViewController {
         val shoot = ShootAction(bulletCounter)
         shoot.shootAction(this)
         shoot.collision(this)
+        bubbles.forEach {
+            it + Actions.forever(CircularMotion(144f, 144f, 2.8f))
+        }
         this + backGround
         this + backgroundBubble
         this + cannon
