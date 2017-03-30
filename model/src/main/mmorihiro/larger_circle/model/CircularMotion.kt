3658cbe0dc61e2b28319caac570e3eda8d300a00
MMorihiro <motakesi54@gmail.com>
2017-03-30 19:25:28 +0900

    Make bubbles fade in/out(#19)

--- model/src/main/mmorihiro/larger_circle/model/CircularMotion.kt
+++ model/src/main/mmorihiro/larger_circle/model/CircularMotion.kt
@@ -1,19 +1,17 @@
 package mmorihiro.larger_circle.model
 
-import com.badlogic.gdx.scenes.scene2d.actions.RelativeTemporalAction
+import com.badlogic.gdx.scenes.scene2d.Actor
 import java.lang.Math.*
 
 
 class CircularMotion(private val centerX: Float,
                      private val centerY: Float,
-                     duration: Float) : RelativeTemporalAction() {
-    init {
-        setDuration(duration)
-    }
-
-    private var percent = 0f
+                     private val times: Int,
+                     private val actor: Actor) {
+    private var count = 0
     private val firstRadian by lazy {
-        atan2((actor.x - centerX).toDouble(), (actor.y - centerY).toDouble())
+        atan2((actor.x + actor.width / 2 - centerX).toDouble(),
+                (actor.y + actor.height / 2 - centerY).toDouble())
     }
     private val radius by lazy {
         val x = actor.x + actor.width / 2 - centerX
@@ -21,15 +19,11 @@ class CircularMotion(private val centerX: Float,
         sqrt((x * x + y * y).toDouble()).toFloat()
     }
 
-    override fun begin() {
-        super.begin()
-        percent = 0f
-    }
-
-    override fun updateRelative(percentDelta: Float) {
-        percent += percentDelta
-        val radian = toRadians(360 * percent.toDouble()) + firstRadian
-        actor.x = centerX + radius * cos(radian).toFloat() - actor.width / 2
-        actor.y = centerY + radius * sin(radian).toFloat() - actor.height / 2
+    fun next(): Pair<Float, Float> {
+        count += 1
+        val radian = toRadians(360.0 * count / times + 90) + firstRadian
+        val x = centerX + radius * cos(radian).toFloat() - actor.width / 2
+        val y = centerY + radius * sin(radian).toFloat() - actor.height / 2
+        return x to y
     }
 }
