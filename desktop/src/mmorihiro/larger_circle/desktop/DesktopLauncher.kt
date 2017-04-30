78ea74562aeb92ea61dfbf19b729b2d92befc31b
MMorihiro <motakesi54@gmail.com>
2017-04-30 17:38:06 +0900

    Add loading screen(#40)

--- desktop/src/mmorihiro/larger_circle/desktop/DesktopLauncher.kt
+++ desktop/src/mmorihiro/larger_circle/desktop/DesktopLauncher.kt
@@ -2,6 +2,7 @@ package mmorihiro.larger_circle.desktop
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.graphics.Color
 import mmorihiro.larger_circle.controller.MainListener
 import mmorihiro.larger_circle.model.Values
 
@@ -11,6 +12,8 @@ object DesktopLauncher {
             backgroundFPS = 60
             width = Values.width.toInt()
             height = Values.height.toInt()
+            title = "BubbleRoad"
+            initialBackgroundColor = Color.WHITE
             resizable = false
         }
         
