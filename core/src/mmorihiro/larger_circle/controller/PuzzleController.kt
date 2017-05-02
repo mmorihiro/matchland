3fb99cbaf81391a1b8da0fd60c7f284bd24279ff
MMorihiro <motakesi54@gmail.com>
2017-05-02 21:37:52 +0900

    Fix bug(#40)

--- core/src/mmorihiro/larger_circle/controller/PuzzleController.kt
+++ core/src/mmorihiro/larger_circle/controller/PuzzleController.kt
@@ -14,64 +14,77 @@ class PuzzleController(
     override val view = PuzzleView().apply {
         this + backGround
         this + puzzleBackGround
-        bubbles.forEachIndexed { index, row ->
-            row.forEach {
-                bubbleGroup += it
-                if (index != 4)
-                    it + (delay(index * 0.9f)
-                            then parallel(
-                            moveBy(0f, -tileSize * 4f, 0.9f,
-                                    Interpolation.bounceOut),
-                            forever(Actions.run {
-                                if (it.y <= tileSize * 4) it.alpha = 1f
-                            })))
-            }
-
-            row.first() + (delay(3.7f) then Actions.run {
-                row.forEach { it.clearActions() }
-                moveAction(this, row)
-                touchAction(this, row)
-            })
-        }
         this + bubbleGroup
         this + bar
         this + cover
         cover + (delay(3.8f) then Actions.run { this - cover })
+        bubbles.forEachIndexed { index, row ->
+            row.forEach { bubble ->
+                bubbleGroup += bubble
+                if (index != 4) {
+                    bubble + (delay(index * 0.9f)
+                            then moveBy(0f, -tileSize * 4f, 0.9f,
+                            Interpolation.bounceOut)) +
+                            action {
+                                if (bubble.y <= tileSize * 4) {
+                                    bubble.alpha = 1f
+                                    true
+                                } else false
+                            }
+                }
+                bubble + (delay(3.8f) then Actions.run {
+                    moveAction(this, bubble)
+                    touchAction(this, bubble)
+                })
+            }
+        }
         onEveryAct {
             if (bubbles.first().isEmpty()) {
                 nextRow().let {
-                    it.forEach { bubble -> bubbleGroup += bubble }
-                    moveAction(this, it)
-                    touchAction(this, it)
+                    it.forEach { bubble ->
+                        bubbleGroup += bubble
+                        moveAction(this, bubble)
+                        touchAction(this, bubble)
+                    }
                 }
             }
         }
     }
 
-    private fun touchAction(view: PuzzleView, row: List<Bubble>) = view.run {
-        row.forEach { bubble ->
-            bubble.onClick { _, _ ->
-                val points = bubbles.mapIndexed { yIndex, row ->
-                    row.mapIndexed {
-                        xIndex, bubble ->
-                        (xIndex to yIndex) to bubble.type
-                    }
-                            .filter { it.second == bubble.type }
-                            .filterNot { it.first.second == 4 }
-                            .map { it.first }
-                }.flatten()
-                if (bubble.alpha != 0f) {
-                    val group = PuzzleModel().sameTypeGroup(points).find {
-                        it.contains(((bubble.x - 8) / tileSize).toInt()
-                                to ((bubble.y - 8) / tileSize).toInt())
-                    }!!.filter { (x, y) -> bubbles[y][x].alpha != 0f }.toSet()
-                    group.forEach { (x, y) ->
-                        bubbles[y][x] + fadeOut(0.2f)
-                    }
-                    showAction(view, bubble.type, group.size)
+    private fun touchAction(view: PuzzleView, bubble: Bubble) = view.run {
+        bubble.onClick { _, _ ->
+            val points = bubbles.mapIndexed { yIndex, row ->
+                row.mapIndexed {
+                    xIndex, bubble ->
+                    (xIndex to yIndex) to bubble.type
                 }
+                        .filter { it.second == bubble.type }
+                        .filterNot { it.first.second == 4 }
+                        .map { it.first }
+            }.flatten()
+            if (bubble.alpha != 0f) {
+                val group = PuzzleModel().sameTypeGroup(points).find {
+                    it.contains(((bubble.x + 8) / tileSize).toInt()
+                            to ((bubble.y + 8) / tileSize).toInt())
+                }!!.filter { (x, y) -> bubbles[y][x].alpha != 0f }.toSet()
+                group.forEach { (x, y) ->
+                    bubbles[y][x] + fadeOut(0.2f)
+                }
+                showAction(view, bubble.type, group.size)
             }
         }
+    }
+
+    private fun moveAction(view: PuzzleView, bubble: Bubble) = view.run {
+        bubble + forever(delay(1.2f) then Actions.run {
+            if (cover !in this) {
+                bubble.y -= 48f
+            }
+            if (bubble.y < 0) {
+                bubble.remove()
+                removeBubble(bubble)
+            }
+        })
     }
 
     private fun showAction(view: PuzzleView,
@@ -95,21 +108,6 @@ class PuzzleController(
 
     private fun fadeAction() =
             delay(0.7f) then parallel(fadeOut(0.3f), moveBy(0f, 10f, 0.3f))
-
-    private fun moveAction(view: PuzzleView, row: List<Bubble>) = view.run {
-        row.forEach { bubble ->
-            //  bubbleGroup += bubble
-            bubble + forever(delay(1.2f) then Actions.run {
-                if (cover !in this) {
-                    bubble.y -= 48f
-                }
-                if (bubble.y < 0) {
-                    bubble.remove()
-                    removeBubble(bubble)
-                }
-            })
-        }
-    }
 
     fun resume() {
         view - view.cover
