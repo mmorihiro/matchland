0f2ca1e308ae3ec2d0fc192ea8f5586564cb2545
MMorihiro <motakesi54@gmail.com>
2017-06-19 20:55:06 +0900

    Cut the first effect(#45)

--- core/src/mmorihiro/larger_circle/controller/PuzzleController.kt
+++ core/src/mmorihiro/larger_circle/controller/PuzzleController.kt
@@ -1,12 +1,6 @@
 package mmorihiro.larger_circle.controller
 
-import com.badlogic.gdx.math.Interpolation
-import com.badlogic.gdx.scenes.scene2d.actions.Actions
-import com.badlogic.gdx.scenes.scene2d.actions.Actions.*
-import ktx.actors.alpha
-import ktx.actors.minus
 import ktx.actors.plus
-import ktx.actors.then
 import mmorihiro.larger_circle.view.PuzzleView
 
 
@@ -15,32 +9,14 @@ class PuzzleController(
     override val view = PuzzleView(
             ::touchAction,
             ::onTouchDragged,
-            { view -> onTouchUp(view, this::showAction) }).apply {
+            { view -> onTouchUp(view, { _, _, _ -> TODO() }) }).apply {
         this + backGround
         this + puzzleBackGround
         this + bubbleGroup
-        this + bar
-        this + cover
-        cover + (delay(3.8f) then Actions.run { this - cover })
-        bubbles.forEachIndexed { index, row ->
-            row.forEach { bubble ->
-                bubbleGroup += bubble
-                if (index != 4) {
-                    bubble + (delay(index * 0.9f)
-                            then moveBy(0f, -tileSize * 4f, 0.9f,
-                            Interpolation.bounceOut)) +
-                            action {
-                                if (bubble.y <= tileSize * 4) {
-                                    bubble.alpha = 1f
-                                    true
-                                } else false
-                            }
-                }
-            }
-        }
+        bubbles.forEach { it.forEach { bubbleGroup + it } }
     }
 
-    private fun showAction(view: PuzzleView,
+    /*private fun showAction(view: PuzzleView,
                            type: Pair<Int, Int>, size: Int): Unit = view.run {
         cover.alpha = 0f
         this + cover
@@ -61,9 +37,9 @@ class PuzzleController(
     }
 
     private fun fadeAction() =
-            delay(0.7f) then parallel(fadeOut(0.3f), moveBy(0f, 10f, 0.3f))
+            delay(0.7f) then parallel(fadeOut(0.3f), moveBy(0f, 10f, 0.3f))*/
 
     fun resume() {
-        view - view.cover
+
     }
 }
