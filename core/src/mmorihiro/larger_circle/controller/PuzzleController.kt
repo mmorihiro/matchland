ebb98cce564ab91281ac31e1acc49641c2fe5142
MMorihiro <motakesi54@gmail.com>
2017-07-15 18:16:48 +0900

    Remove connected bubbles and add new ones(#45)

--- core/src/mmorihiro/larger_circle/controller/PuzzleController.kt
+++ core/src/mmorihiro/larger_circle/controller/PuzzleController.kt
@@ -1,5 +1,9 @@
 package mmorihiro.larger_circle.controller
 
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.math.Interpolation
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import ktx.actors.alpha
 import ktx.actors.plus
 import mmorihiro.larger_circle.view.PuzzleView
 
@@ -9,7 +13,7 @@ class PuzzleController(
     override val view = PuzzleView(
             ::touchAction,
             ::onTouchDragged,
-            { view -> onTouchUp(view, { _, _, _ -> TODO() }) }).apply {
+            { view -> onTouchUp(view, { _, _, _ -> addNewBubbles() }) }).apply {
         this + backGround
         this + puzzleBackGround
         this + bubbleGroup
@@ -38,6 +42,34 @@ class PuzzleController(
 
     private fun fadeAction() =
             delay(0.7f) then parallel(fadeOut(0.3f), moveBy(0f, 10f, 0.3f))*/
+
+    fun addNewBubbles(): Unit = view.run {
+        bubbles = bubbles.mapIndexed { xIndex, it ->
+            val remains = it.filter { it.color == Color.WHITE }
+            if (remains.size == 4) remains
+            else remains + (0..3 - remains.size).map {
+                loader.loadRandom().apply {
+                    x = xIndex * tileSize + 8
+                    y = tileSize * (4 + it)
+                    alpha = 0f
+                    this + Actions.fadeIn(0.15f)
+                    bubbleGroup + this
+                }
+            }
+        }
+        moveToRightPoint()
+    }
+
+    private fun moveToRightPoint() = view.run {
+        bubbles.forEach {
+            it.forEachIndexed { index, bubble ->
+                val toY = index * tileSize + 8
+                bubble + Actions.moveTo(bubble.x, toY,
+                        0.3f * Math.abs(bubble.y - toY) / tileSize,
+                        Interpolation.swingOut)
+            }
+        }
+    }
 
     fun resume() {
 
