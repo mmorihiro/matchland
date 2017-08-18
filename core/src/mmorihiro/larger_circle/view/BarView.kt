8460de72966021513fa0e23df940766d742aa412
MMorihiro <motakesi54@gmail.com>
2017-08-18 16:59:40 +0900

    Three stars on bar(#64)

--- core/src/mmorihiro/larger_circle/view/BarView.kt
+++ core/src/mmorihiro/larger_circle/view/BarView.kt
@@ -3,6 +3,7 @@ package mmorihiro.larger_circle.view
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import ktx.assets.asset
+import mmorihiro.larger_circle.model.Point
 import mmorihiro.larger_circle.model.Values
 
 
@@ -13,6 +14,29 @@ class BarView : View() {
         color = Colors.fire
     }
     val barWidth = bar.width
+    var stars = listOf(50, 100, 140).map {
+        getStar(StarType.GRAY).apply {
+            x = it.toFloat() + bar.x
+            y = 8f
+        }
+    }
+
+    fun getStar(type: StarType) =
+            MyImage(Image(asset<Texture>(type.fileName)), type.position)
 
     fun getPercentWidth(value: Int): Float = barWidth * value / 35f
+}
+
+enum class StarType {
+    GRAY {
+        override val position = 0 to 0
+        override val fileName = "grayStar.png"
+    },
+    GET {
+        override val position = 0 to 1
+        override val fileName = "star.png"
+    };
+
+    abstract val position: Point
+    abstract val fileName: String
 }
