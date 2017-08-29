3c445d654c8477e7b6690ca8de315695fcbf45b5
MMorihiro <motakesi54@gmail.com>
2017-08-29 17:21:45 +0900

    Use Gdx.files(#75)

--- model/src/main/mmorihiro/matchland/model/ConfigModel.kt
+++ model/src/main/mmorihiro/matchland/model/ConfigModel.kt
@@ -1,27 +1,27 @@
 package mmorihiro.matchland.model
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.squareup.moshi.Moshi
-import java.io.File
 
 
 object ConfigModel {
-    private val file = File("config.json")
+    private val file = Gdx.files.local("config.json")
     val moshi: Moshi = Moshi.Builder().build()
     private val adapter = moshi.adapter(Config::class.java)
     var config =
-            if (file.exists()) adapter.fromJson(file.readText())!!
+            if (file.exists()) adapter.fromJson(file.readString())!!
             else Config(1, ItemType.FIRE)
         private set
 
     fun onStageChange() {
         config = config.copy(stageNumber = config.stageNumber + 1)
-        file.writeText(adapter.toJson(config))
+        file.writeString(adapter.toJson(config), false)
     }
     
     fun onItemTypeChange(color: Color) {
         config = config.copy(itemType = ItemType.values().first { it.color == color })
-        file.writeText(adapter.toJson(config))
+        file.writeString(adapter.toJson(config), false)
     }
 
     data class Config(val stageNumber: Int, val itemType: ItemType)
