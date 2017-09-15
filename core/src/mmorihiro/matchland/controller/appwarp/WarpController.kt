7e10f1e705a7a3fe78581bcf6e82802f3de1ab78
forestone23 <forestone23@users.noreply.github.com>
2017-09-15 20:34:49 +0900

    Add error handler in lobby

--- core/src/mmorihiro/matchland/controller/appwarp/WarpController.kt
+++ core/src/mmorihiro/matchland/controller/appwarp/WarpController.kt
@@ -4,8 +4,11 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.shephertz.app42.gaming.multiplayer.client.WarpClient
+import com.shephertz.app42.gaming.multiplayer.client.command.WarpResponseResultCode.*
 import com.shephertz.app42.gaming.multiplayer.client.events.LiveRoomInfoEvent
 import com.squareup.moshi.Types
+import de.tomgrill.gdxdialogs.core.GDXDialogsSystem
+import de.tomgrill.gdxdialogs.core.dialogs.GDXButtonDialog
 import ktx.actors.plus
 import ktx.assets.asset
 import mmorihiro.matchland.controller.*
@@ -19,9 +22,11 @@ import mmorihiro.matchland.view.View
 import java.lang.reflect.Type
 
 class WarpController(private val onStart: () -> Unit,
-                     onHome: () -> Unit, top: View) : Controller {
+                     private val onHome: () -> Unit, private val top: View) : Controller {
     val warpClient: WarpClient by lazy { WarpClient.getInstance() }
     lateinit var roomID: String
+    private val dialogs = GDXDialogsSystem.install()
+    private var errorDialogShown = false
     override val view = WarpPuzzleView(
             onTouchDown = { view, x, y ->
                 touchAction(view, x, y)
@@ -68,7 +73,7 @@ class WarpController(private val onStart: () -> Unit,
                 "ef2760dcfdbbff89c6d081934e985c26cc05e16dc026146b972ca1a1ad3fc9fc")
         warpClient.addConnectionRequestListener(ConnectionListener(warpClient))
         warpClient.addRoomRequestListener(RoomListener(this))
-        warpClient.addZoneRequestListener(ZoneListener(warpClient))
+        warpClient.addZoneRequestListener(ZoneListener(this, warpClient))
         warpClient.addNotificationListener(NotificationListener(this))
         warpClient.connectWithUserName(view.playerType.name)
     }
@@ -140,5 +145,41 @@ class WarpController(private val onStart: () -> Unit,
 
         warpClient.sendUpdatePeers("${ConfigModel.config.itemType
                 .name}@${MessageType.NewItem.name}@$str".toByteArray())
+    }
+
+    internal fun onLobbyError(where: String?, result: Byte) {
+        if(errorDialogShown) return
+        else errorDialogShown = true
+
+        warpClient.leaveLobby()
+        warpClient.disconnect()
+
+        val (resultCode, resultText) = when(result) {
+            AUTH_ERROR -> "AUTH_ERROR" to "The session id sent in the request was incorrect. This can happen if the client connects without initializing with the correct keys."
+            RESOURCE_MOVED -> "RESOURCE_MOVED" to "The resource for which the request was intended to has moved to a state where it can’t process the request. For example, if a client sends a chat or updatePeers message and the connected user is not present in any room."
+            UNKNOWN_ERROR -> "UNKNOWN_ERROR" to "This is an unexpected error. Retrying the request is recommended if this happens."
+            BAD_REQUEST -> "BAD_REQUEST" to "This occurs if the parameters passed to the request are invalid. For example if null or empty string is passed in the roomId parameter of a joinRoom request."
+            CONNECTION_ERROR -> "CONNECTION_ERROR" to "This occurs if the underlying TCP connection with AppWarp cloud service got broken. The client will need to reconnect to the service and retry the operation."
+            SUCCESS_RECOVERED -> "SUCCESS_RECOVERED" to "This occurs if when successfully recover a session."
+            CONNECTION_ERROR_RECOVERABLE -> "CONNECTION_ERROR_RECOVERABLE" to "This occurs if the underlying TCP connection with AppWarp cloud service got broken but same session can be recovered. The client will need call RecoverConnection() to resume same session."
+            USER_PAUSED_ERROR -> "USER_PAUSED_ERROR" to ""
+            AUTO_RECOVERING -> "AUTO_RECOVERING" to ""
+            else -> "Unknown error " + result to ""
+        }
+
+        val bDialog = dialogs.newDialog(GDXButtonDialog::class.java)
+        bDialog.setTitle("Error")
+        bDialog.setMessage("Failed to connect to server($where): $resultCode\n$resultText")
+        bDialog.addButton("OK")
+
+        bDialog.setClickListener {
+            errorDialogShown = false
+            Gdx.app.postRunnable {
+                onHome()
+                StageChangeEffect().resumeEffect(top)
+            }
+        }
+
+        bDialog.build().show()
     }
 }
